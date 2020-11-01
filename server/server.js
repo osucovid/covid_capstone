db6d543a69977cfa4ffbd0884fed7eab51d62a83
chosun <chosun@oregonstate.edu>
2020-11-01 22:20:47 +0900

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,50 @@
+// server/server.js
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// mock data to send to our frontend
+let events = [
+  {
+    id: 1,
+    name: "Charity Ball",
+    category: "Fundraising",
+    description:
+      "Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.",
+    featuredImage: "https://placekitten.com/500/500",
+    images: [
+      "https://placekitten.com/500/500",
+      "https://placekitten.com/500/500",
+      "https://placekitten.com/500/500",
+    ],
+    location: "1234 Fancy Ave",
+    date: "12-25-2019",
+    time: "11:30",
+  },
+  {
+    id: 2,
+    name: "Rescue Center Goods Drive",
+    category: "Adoptions",
+    description:
+      "Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.",
+    featuredImage: "https://placekitten.com/500/500",
+    images: ["https://placekitten.com/500/500"],
+    location: "1234 Dog Alley",
+    date: "11-21-2019",
+    time: "12:00",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send(`Hi! Server is listening on port ${port}`);
+});
+
+// listen on the port
+app.listen(port);
