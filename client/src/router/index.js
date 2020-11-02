0155da67d8f661f773938d4cec0e3e120d6d9214
chosun <chosun@oregonstate.edu>
2020-11-02 21:52:14 +0900

    Removing auth in Today's Assessment Data for testing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -28,7 +28,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/TodaysData.vue"),
-    beforeEnter: authGuard,
+    // beforeEnter: authGuard,
   },
   {
     path: "/riskgraph/:id",
