cef993474ad68443598f1d17ef74fc2ac7536691
chosun <chosun@oregonstate.edu>
2020-11-02 22:01:07 +0900

    Update index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import { authGuard } from "../auth/authGuard";
+// import { authGuard } from "../auth/authGuard";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
