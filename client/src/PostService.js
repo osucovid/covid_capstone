0f640a76b3f048a948c6ba6e7f927f29e68bf369
chosun <chosun@oregonstate.edu>
2020-11-02 20:55:24 +0900

    Moving express to root for Heroku

---
+++ client/src/PostService.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+
+const url = "api/posts/";
+
+class PostService {
+  //get posts
+  //using static means that even though we have a class and a method
+  //we don't have to instantiate a new PostService object to use it
+  //we can simply use PostService.getPosts()
+
+  //commented out because async psomise executor functions are not considered good practice
+  //forced by eslint
+  // static getPosts() {
+  //     return new Promise( (resolve, reject) => {
+  //         try {
+  //             const res = await axios.get(url);
+  //             const data = res.data;
+  //             resolve(
+  //                 data.map(post => ({
+  //                     ...post,
+  //                     createdAt: new Date(post.createdAt)
+  //                 }))
+  //             );
+  //         } catch(err) {
+  //             reject(err);
+  //         }
+  //     })
+  // }
+  static getPosts() {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(url)
+        .then((res) => {
+          const data = res.data;
+          resolve(
+            data.map((post) => ({
+              ...post,
+              createdAt: new Date(post.createdAt),
+            }))
+          );
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  //create post
+  static insertPost(text) {
+    return axios.post(url, {
+      text,
+    });
+  }
+
+  //delete post
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
+  }
+}
+
+export default PostService;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-// server/server.js
+// /server/server.js
+// for express
 const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
-// const port = 8000;
-const port = process.env.PORT || 8000;
+// const path = require("path");
 
-app.use(bodyParser.json());
+//initialize app with express
+const app = express();
+
+// middleware for dependencies
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(serveStatic(path.join(__dirname, "dist")));
+
+//a simple crud function for posting
+const posts = require("./routes/api/posts");
+// const users = require("./routes/api/users");
+
+app.use("/api/posts", posts);
 
 // mock data to send to our frontend
 let events = [
@@ -46,9 +49,17 @@ let events = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send(`Hi! Server is listening on port ${port}`);
-});
+//handle production
+if (process.env.NODE_ENV === "production") {
+  //static folder - __dirname = current directory  and concatenate instead of using path module
+  //look at that folder as our static folder
+  app.use(express.static(__dirname + "/public/"));
 
-// listen on the port
-app.listen(port);
+  //handle spa - any route
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
+//port for heroku, localhost will run on 5000
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
