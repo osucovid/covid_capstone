831b765718783b8d496266ab70bd075739636992
byco <bky.chao@gmail.com>
2020-11-21 22:25:20 -0800

    add content to new homepage

---
+++ client/src/components/Intro.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id = "container">
+  <div id = "text">
+    <h1>Curious about your risk for catching <strong>COVID-19</strong>?</h1>
+
+      <p>With so much brand (and often conflicting) new information about the COVID-19 pandemic, people are overwhelmed with the amount of data and advice provided by the media.</p>
+
+      <p><strong>Fill out a few questions and our app will help you evaluate your risk for COVID-19 based on your past and present behavior.</strong> We will compare your activity with publicly available data and current recommendations from researchers and public health departments.</p>
+
+      <p><a href="/todaysdata">Begin by registering for account or filling out a daily assessment form</a>!</p>
+  </div>
+
+  <div id = "break"></div>
+
+  <div id = "text">
+  <h2>It's easy to get your assessment!</h2>
+
+  <div id = "card-container">
+  <div id = "card">
+    <img
+      :src="require('../assets/form-icon.svg')"
+      :width="75"
+    />
+    <p>Fill out questions about your medical history, social distancing efforts, and recent activity.</p>
+  </div>
+
+  <div id = "card">
+    <img
+      :src="require('../assets/bar-chart.svg')"
+      :width="75"
+    />
+    <p>Get instantaneous results about how you compare to high-risk COVID patients.</p>
+  </div>
+
+  <div id = "card">
+    <img
+      :src="require('../assets/stethoscope.svg')"
+      :width="75"
+    />
+    <p>Learn more about the pandemic and how to lower your risk, in consultation with your doctors.</p>
+  </div>
+  </div>
+  </div>
+  </div>
+</template>
+
+<style>
+#container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+#text {
+  padding: 25px;
+  background-color: #ddd;
+  flex-basis: 60%;
+}
+
+#break {
+  flex-basis: 100%;
+  height: 0;
+}
+
+#card-container {
+  display: flex;
+}
+
+#card {
+  flex-grow: 1;
+  padding: 30px;
+  border: 10px solid #ddd;
+  background-color: #eee;
+}
+
+@media only screen and (min-width: 600px) {
+
+}
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,34 +1,17 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
-    <!-- Main is Main.vue which contains the Dashboard with the graph and Risk Assessment -->
-    <Dashboard />
-    <!-- <Map /> -->
-    <RiskStatus />
-    <JsonMap2 />
+    <Intro />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-// Main contains the main dashbord
-//import Dashboard from "@/components/Dashboard.vue";
-//import RiskStatus from "@/components/RiskStatus.vue";
-// import Map from "@/components/Map.vue";
-//import JsonMap2 from "@/components/JsonMap2.vue";
+import Intro from "@/components/Intro.vue";
 
 export default {
   name: "Home",
   components: {
-    // HelloWorld
-    //Dashboard,
-    //RiskStatus,
-    // Map,
-    //JsonMap2,
+    Intro
   },
 };
 </script>
