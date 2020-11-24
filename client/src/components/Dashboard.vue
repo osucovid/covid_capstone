d329a375301faa96058a3cbeede1ea6e7a93bfb2
byco <bky.chao@gmail.com>
2020-11-23 19:57:08 -0800

    add health conditions and mask wearing question viz

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -48,16 +48,76 @@
       </b-card-group>
       <br />
 
-      <button type="button" class="btn btn-secondary btn-lg">
-        Button Example
-      </button>
+ <form>
+
+  <!--- Input section for various health conditions --->
+  <input type="checkbox" id="condition1">
+  <label for="condition1"> I have Cancer</label><br>
+
+  <input type="checkbox" id="condition2">
+  <label for="condition2"> I have Chronic Kidney Disease</label><br>
+
+  <input type="checkbox" id="condition3">
+  <label for="condition3"> I have COPD</label><br>
+
+  <input type="checkbox" id="condition4">
+  <label for="condition4"> I have Heart Condition(s)</label><br>
+
+  <input type="checkbox" id="condition5">
+  <label for="condition5"> I am Immunocompromised</label><br>
+
+  <input type="checkbox" id="condition6">
+  <label for="condition6"> I am Obese</label><br>
+
+  <input type="checkbox" id="condition7">
+  <label for="condition7"> I have Sickle Cell Disease</label><br>
+
+  <input type="checkbox" id="condition8">
+  <label for="condition8"> I am a Smoker</label><br>
+
+  <input type="checkbox" id="condition9">
+  <label for="condition9"> I have Type 2 Diabetes</label><br>
+
+</form>
+
+<div id="bubbles"></div>
+
       <br />
     </div>
   </div>
+
 </template>
 
+<script src="http://d3js.org/d3.v6.min.js" charset="utf-8"></script>
+
 <script>
+import * as d3 from 'd3'
 export default {
+  data () {
+    return {}
+  },
+  mounted () {
+
+  },
   name: "Dashboard",
 };
+
 </script>
+
+<style scoped>
+</style>
+
+<style>
+.chart div {
+  font: 10px sans-serif;
+  background-color: steelblue;
+  text-align: right;
+  padding: 3px;
+  margin: 1px;
+  color: white;
+}
+.container-chart {
+  /* width: 50%; */
+  margin: auto;
+}
+</style>
